6d42fab8828aae159d816d2abfa22cea1c8ffe05
Deepanshu Tiwari <183710540+deepanshutiwari16@users.noreply.github.com>
2025-01-31 00:15:10 +0530

    using classes

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-import { trackingExpense } from "./src/expenses.js";
+import { Cash } from "./src/cash.js";
 
-const main = () => {
-  const input = Deno.args;
-  return trackingExpense(input);
+const main = (allExpense) => {
+  // const allExpense = Deno.readTextFileSync("./Data/money.txt").split("\n");
+
+  const expenses = allExpense.reduce((object, expense) => {
+    const money = Cash.parse(expense);
+
+    return money.add(object);
+  }, new Cash(0, 0));
+
+  return expenses.toString();
 };
 
-console.log(main());
+console.log(main([12.5, 3.45, 1.75, 2.3]));
